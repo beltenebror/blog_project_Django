5b55dce9ace6f99e29bd5a181a56dda40d438b9c
beltenebror <lasupercintia@gmail.com>
2021-02-16 20:23:10 +0100

    punto 7 acabado

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView,  UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 # Create your views here
@@ -13,3 +14,20 @@ class BlogDetailView(DetailView):
     template_name = 'post_detail.html'
     context_object_name = 'post'
 
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    template_name = 'post_edit.html'
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
