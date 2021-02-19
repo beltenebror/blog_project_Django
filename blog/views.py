5d8ce59f87ccd79e50f47aa1eb3951e6046bdf82
beltenebror <lasupercintia@gmail.com>
2021-02-19 11:10:03 +0100

    punto 8 finalizado

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView, DetailView, CreateView,  UpdateView, DeleteView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
+
 
 # Create your views here
 class BlogListView(ListView):
